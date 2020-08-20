86f77392ba456700fad832293753ad82f4458b68
root <afolabimkay@gmail.com>
2020-08-20 06:24:23 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='slik_preprocessing',
+      version='0.1',
+      description='A data preprocessing tool',
+      packages=['slik_preprocessing'],
+      author = 'Adeshola Afolabi',
+      zip_safe=False)
---
+++ slik_preprocessing/__init__.py
@@ -0,0 +1,4 @@
+from .general_utils import Read_file
+from .preprocessing import Clean
+from .pipeline import Build
+from .save_object import Save_pipeline
